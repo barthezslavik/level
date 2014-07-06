3ebe7b1b52fa8ddb4b1307b755fbc44ece1bcf0f
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2014-07-06 15:46:15 +0300

    init commit

---
+++ header.php
@@ -0,0 +1,31 @@
+<div class="header">
+    
+      <div class="logo">
+        <img class="logo" src="images/logo.png" alt="LevelCut">
+        <span class="logo-gray">DAS VOLUMEWUNDER</span>
+        <span class="logo-red">VON VOLLEREM HAAR</span>
+      </div>
+
+      <ul class="lang">
+        <li>Für Friseure:</li>
+        <li><a href="#">Ihr Weg zu Levelcut</a></li>
+        <li><a href="#">Deutsch</a></li>
+        <li><a href="#">English</a></li>
+        <li><a href="#">Niederländisch</a></li>
+      </ul>
+
+      <ul class="top-menu">
+        <li> <a href="#">Home</a> </li>
+        <li> <a href="#">Video</a> </li>
+        <li> <a href="#">Vergleich</a> </li>
+        <li> <a href="#">Schulung</a> </li>
+        <li> <a href="#">Galerie</a> </li>
+        <li> <a href="#">Salons</a> </li>
+        <li> <a href="#">Hermann Wiezorek</a> </li>
+      </ul>
+
+    <div>
+      
+    </div>
+
+    </div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="css/style.css" rel="stylesheet">
+
+    <title>LevelCut</title>
+  </head>
+
+
+
+  <body>
+    <?php require "header.php" ?>
+
+
+  </body>
+
+</html>
